c17c0306dc36766bcee46af7cc3bcb221a1cb9a6
Student <student@mx23-hp>
2025-04-23 16:43:44 +0100

    first commit

---
+++ 02var/main.go
@@ -0,0 +1,13 @@
+package main
+
+import ("fmt")
+
+func main(){
+var student1 string= "John" / /type is explicit string 
+var student2= "Jane" / /type is inferred
+var age 1 int= 28 / /type is explicit int
+age2 := 32 / / type is inferred 
+fmt.Println(student1)
+fmt.Println(student2)
+fmt.Println(age 1)
+fmt.Println(age2)
